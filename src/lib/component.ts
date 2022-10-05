eb297b7bd504ec8240bb6db0c26d9a0d5c6f708e
Orbwebos <97360590+Orbwebos@users.noreply.github.com>
2022-10-05 10:48:06 -0700

    rename `ComponentCompliant` to `ProvidesBCL`

--- src/lib/component.ts
+++ src/lib/component.ts
@@ -2,7 +2,13 @@ import { Client } from 'discord.js';
 import { Base, base } from './base';
 import { Logger } from './logger';
 
-export abstract class Component {
+export interface ProvidesBCL {
+  get base(): Base;
+  get client(): Client;
+  get logger(): Logger;
+}
+
+export abstract class Component implements ProvidesBCL {
   public get base(): Base {
     return base;
   }
@@ -14,10 +20,4 @@ export abstract class Component {
   public get logger(): Logger {
     return base.logger;
   }
-}
-
-export interface ComponentCompliant {
-  get base(): Base;
-  get client(): Client;
-  get logger(): Logger;
 }
